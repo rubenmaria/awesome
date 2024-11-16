51d5a8882b9e903e10b8070964de1f3db8bad510
Ruben <steamkonto60@gmail.com>
2024-11-16 22:46:57 +0100

    created laptop branch

---
+++ scripts/hdmi-laptop-mirror.sh
@@ -0,0 +1,6 @@
+xrandr --output LVDS1 --mode 1366x768  --output HDMI1 \
+  --same-as LVDS1 --mode 1920x1080
+#--scale 1x1
+#xrandr --output LVDS1 --mode 1366x768 \
+#  --fb 1920x1080 --panning 1920x1080* --output HDMI1 \
+#  --mode 1920x1080 --same-as LVDS1
